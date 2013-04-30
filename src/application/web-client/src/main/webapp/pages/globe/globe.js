91e7c10783047a4ed8a6753e8100e0464cfcfa32
markjohndoyle <markjohndoyle@googlemail.com>
2013-04-30 10:21:39 +0200

    Base globe page initial commit.

---
+++ src/application/web-client/src/main/webapp/pages/globe/globe.js
@@ -0,0 +1,28 @@
+
+// Test
+ var czml = 
+[
+	{
+		"id" : "satellite",
+		"availability" : "2013-04-30T00:00:00Z/2013-04-30T21:00:00Z",
+		"position" : { "cartographicDegrees" : [-75.0, 40.0, 0.0] },
+		"billboard" : {
+            "eyeOffset" : {
+                "cartesian" : [0.0, 0.0, 0.0]
+            },
+            "horizontalOrigin" : "CENTER",
+            "image" : "https://dl.dropboxusercontent.com/u/10007025/icons/UFO.png",
+            "pixelOffset" : {
+                "cartesian2" : [0.0, 0.0]
+            },
+            "scale" : 0.8333333333333334,
+            "show" : [{
+                "interval" : "2012-08-04T16:00:00Z/2012-08-04T18:00:00Z",
+                "boolean" : true
+            }],
+            "verticalOrigin" : "BOTTOM"
+        },
+		
+	}
+];
+
