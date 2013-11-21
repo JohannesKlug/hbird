77399ccf7376ca8460985b701e2c6911ed2f94d7
Mark Doyle <markjohndoyle@googlemail.com>
2013-11-21 15:55:05 +0100

    Moved master propagation anonymous handler to a new class file.

---
+++ src/application/space-dynamics/src/main/java/org/hbird/application/spacedynamics/tle/CzmlGeneratorHandler.java
@@ -0,0 +1,65 @@
+package org.hbird.application.spacedynamics.tle;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.hbird.application.spacedynamics.tle.TleCzmlkUtilities.PropagationFinishedListener;
+import org.joda.time.DateTime;
+import org.orekit.errors.OrekitException;
+import org.orekit.errors.PropagationException;
+import org.orekit.frames.Frame;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.propagation.sampling.OrekitFixedStepHandler;
+import org.orekit.time.AbsoluteDate;
+import org.orekit.time.TimeScalesFactory;
+import org.orekit.utils.PVCoordinates;
+
+final class CzmlGeneratorHandler implements OrekitFixedStepHandler {
+	private final PropagationFinishedListener finishedListener;
+
+	private final Frame frame;
+
+	CzmlGeneratorHandler(PropagationFinishedListener finishedListener, Frame frame) {
+		this.finishedListener = finishedListener;
+		this.frame = frame;
+	}
+
+	@Override
+	public void init(SpacecraftState spacecraftState, AbsoluteDate date) throws PropagationException {
+		// Nothing to do
+	}
+
+	@Override
+	public void handleStep(SpacecraftState spacecraftState, boolean isLast) throws PropagationException {
+		createCzmlTimeTaggedPosition(finishedListener, frame, spacecraftState, isLast);
+	}
+
+	private void createCzmlTimeTaggedPosition(final PropagationFinishedListener finishedListener, final Frame frame, SpacecraftState spacecraftState, boolean isLast) {
+		PVCoordinates coords;
+		try {
+			coords = spacecraftState.getPVCoordinates(frame);
+		}
+		catch (OrekitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		DateTime isoDate;
+		try {
+			isoDate = new DateTime(spacecraftState.getDate().toDate(TimeScalesFactory.getUTC()));
+		}
+		catch (OrekitException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		Vector3D pos = coords.getPosition();
+		if (isLast) {
+			System.out.println("\"" + isoDate + "\"" + ", " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ());
+			if (finishedListener != null) {
+				finishedListener.finished();
+			}
+		}
+		else {
+			System.out.println("\"" + isoDate + "\"" + ", " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ() + ",");
+		}
+	}
+}
--- src/application/space-dynamics/src/main/java/org/hbird/application/spacedynamics/tle/TleCzmlkUtilities.java
+++ src/application/space-dynamics/src/main/java/org/hbird/application/spacedynamics/tle/TleCzmlkUtilities.java
@@ -4,23 +4,17 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.hbird.application.spacedynamics.tle.TleCzmlkUtilities.PropagationFinishedListener;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.orekit.errors.OrekitException;
-import org.orekit.errors.PropagationException;
 import org.orekit.frames.Frame;
 import org.orekit.frames.FramesFactory;
-import org.orekit.propagation.SpacecraftState;
 import org.orekit.propagation.analytical.tle.TLE;
 import org.orekit.propagation.analytical.tle.TLEPropagator;
-import org.orekit.propagation.sampling.OrekitFixedStepHandler;
 import org.orekit.time.AbsoluteDate;
 import org.orekit.time.TimeScalesFactory;
-import org.orekit.utils.PVCoordinates;
 
 /**
  * 
@@ -31,7 +25,7 @@ import org.orekit.utils.PVCoordinates;
 public class TleCzmlkUtilities {
 
 	private static final int HALF_MINUTE = 30;
-	
+
 	/** The remainder given by the modulus when we have read a TLE for a satellite */
 	private static final int SAT_NAME_LINE_ORDINAL = 0;
 
@@ -43,8 +37,7 @@ public class TleCzmlkUtilities {
 	 * @throws IOException
 	 * @throws OrekitException
 	 */
-	public static final String createCzmlFromTleFile(File tleFile, final PropagationFinishedListener finishedListener, String... satNames) throws IOException,
-			OrekitException {
+	public static final String createCzmlFromTleFile(File tleFile, final PropagationFinishedListener finishedListener, String... satNames) throws IOException, OrekitException {
 		String czml = null;
 
 		BufferedReader bufRead = new BufferedReader(new FileReader(tleFile));
@@ -74,50 +67,7 @@ public class TleCzmlkUtilities {
 				final AbsoluteDate now = new AbsoluteDate(nowUtc.toDate(), TimeScalesFactory.getUTC());
 				final AbsoluteDate tomorrow = new AbsoluteDate(tomorrowUtc.toDate(), TimeScalesFactory.getUTC());
 
-				proper.setMasterMode(HALF_MINUTE, new OrekitFixedStepHandler() {
-
-					@Override
-					public void init(SpacecraftState spacecraftState, AbsoluteDate date) throws PropagationException {
-//						createCzmlTimeTaggedPosition(finishedListener, frame, spacecraftState, false);
-					}
-
-					@Override
-					public void handleStep(SpacecraftState spacecraftState, boolean isLast) throws PropagationException {
-						createCzmlTimeTaggedPosition(finishedListener, frame, spacecraftState, isLast);
-					}
-
-					private void createCzmlTimeTaggedPosition(final PropagationFinishedListener finishedListener, final Frame frame,
-							SpacecraftState spacecraftState, boolean isLast) {
-						PVCoordinates coords;
-						try {
-							coords = spacecraftState.getPVCoordinates(frame);
-						}
-						catch (OrekitException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-							return;
-						}
-						DateTime isoDate;
-						try {
-							isoDate = new DateTime(spacecraftState.getDate().toDate(TimeScalesFactory.getUTC()));
-						}
-						catch (OrekitException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-							return;
-						}
-						Vector3D pos = coords.getPosition();
-						if (isLast) {
-							System.out.println("\"" + isoDate + "\"" + ", " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ());
-							if (finishedListener != null) {
-								finishedListener.finished();
-							}
-						}
-						else {
-							System.out.println("\"" + isoDate + "\"" + ", " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ() + ",");
-						}
-					}
-				});
+				proper.setMasterMode(HALF_MINUTE, new CzmlGeneratorHandler(finishedListener, frame));
 
 				proper.propagate(now, tomorrow);
 
