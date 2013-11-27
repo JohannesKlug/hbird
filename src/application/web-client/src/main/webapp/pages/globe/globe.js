c35c26fcb6419452cfec6804cdaab46a73ba67cd
Mark Doyle <markjohndoyle@googlemail.com>
2013-11-27 10:29:58 +0100

    Added web socket connection to hbird in the globe.

--- src/application/web-client/src/main/webapp/pages/globe/globe.js
+++ src/application/web-client/src/main/webapp/pages/globe/globe.js
@@ -3,10 +3,9 @@ var host = window.location.hostname;
 var url = "/hbird/halcyon/";
 var rootURL = location.protocol + "//" + host + ":" + location.port + url;
 
-// Test
 var czml;
-
 var cesiumViewer;
+var liveTmWebsocket;
 
 /**
  * On page ready do the following.
@@ -14,6 +13,7 @@ var cesiumViewer;
 jQuery(document).ready(function() {
 	setupGlobe();
 	loadCzml();
+	setupWebsocket();
 });
 
 function loadCzml() {
@@ -44,4 +44,52 @@ function setupGlobe() {
     
 	// For dynamic object camera lock on
 	cesiumViewer.extend(Cesium.viewerDynamicObjectMixin);
+}
+
+function parameterReceived(parameter) {
+	
+}
+
+/**
+ * Sets up the web socket and it's callbacks.
+ */
+function setupWebsocket() {
+	var wsProtocol;
+
+	if (location.protocol == "http:") {
+		wsProtocol = "ws:";
+	} else {
+		wsProtocol = "wss:";
+	}
+
+	liveTmWebsocket = $.gracefulWebSocket(wsProtocol + "//" + host + ":" + location.port + url + "websocket");
+
+	liveTmWebsocket.onopen = function() {
+		$.pnotify({
+		    title: "System message",
+		    text: "Websocket connection established.",
+		    type: "info",
+		    icon: "'picon picon-network-wireless'"
+		});
+	};
+	
+	liveTmWebsocket.onerror = function() {
+		$.pnotify({
+		    title: "System message",
+		    text: "Websocket connection failed. Cannot receive live telemetry.",
+		    type: "error",
+		    icon: "'picon picon-network-wireless'"
+		});
+	};
+
+	// When we receive a message from the websocket first check if there are any widgets on the dashboard,
+	// if so, parse the message into a parameter object and call our handler.
+	liveTmWebsocket.onmessage = function(event) {
+		var message = $.parseJSON(event.data);
+		if(message.id === "LIVE_TM") {
+			if(Object.keys(hidgetMonitorMap).length > 0) {
+				parameterReceived(message.content);
+			}
+		}
+	};
 }
