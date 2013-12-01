ef19dbab36d555bb04f5fdf5caf7b4cd1837d327
Mark Doyle <markjohndoyle@googlemail.com>
2013-12-01 19:51:35 +0100

    Improved AMD defines and requires. Modules properly encapsulated, dependencies properly pathed and shimmed. Added app menu trigger with icon. Cleaned up HTML.

--- src/application/web-client/src/main/webapp/js/hbird/rootMenu.js
+++ src/application/web-client/src/main/webapp/js/hbird/rootMenu.js
@@ -1,58 +1,54 @@
-var MENU_PLACEHOLDER = "hbirdRootMenu";
-var MENU_TRIGGER = "menu-trigger";
-
-var rootMenu;
-var menuList;
-
 /**
  * Defines the hbird rootMenu module.
  * Loads the jpanel Javascript and creates the hbird menu on the page.
  */
 define(["jquery", "jpanelmenu/jquery.jpanelmenu.min" ], function($, jPanel) {
+	var MENU_PLACEHOLDER = "hbirdRootMenu";
+	var MENU_TRIGGER = "menu-trigger";
+
+	var rootMenu = null;
+	var menuList = null;
+
+	function createAndAddMenuTrigger() {
+		$("<img id=hbirdMenuTrigger class=" + MENU_TRIGGER + " src='../../images/metro/appbar.list.png' />").prependTo($("body"));
+	}
+
+	/**
+	 * Creates a placeholder div for the menu under the id MENU_PLACEHOLER
+	 */
+	function createAndAddMenuPlaceholder() {
+		var menu = $("<div id=" + MENU_PLACEHOLDER + " style='display: none; position: fixed; top: 64px; left: 4px;' />").prependTo($("body"));
+		menuList = $("<ul>").appendTo(menu);
+	}
+
+	/**
+	 * Creates, adds, and switches on the jpanel menu to the MENU_PLACEHOLDER div.
+	 */
+	function attachMenu() {
+		rootMenu = $.jPanelMenu({
+			menu : "#" + MENU_PLACEHOLDER
+		});
+	}
+
+	function addMenuItem(jqObj, evtHandler) {
+		console.log("Adding menu " + jqObj);
+		jqObj.click(evtHandler);
+		menuList.append($("<li>").append(jqObj));
+	}
+	
 	createAndAddMenuTrigger();
 	createAndAddMenuPlaceholder();
 	attachMenu();
 	console.log("Hbird menu initialised");
 	
 	return {
-		addMenuItem : function(htmlElementMenuItem) {
-			addMenuItem(htmlElementMenuItem);
+		addMenuItem : function(jquerySelector, eventHandler) {
+			addMenuItem(jquerySelector, eventHandler);
 		},
 		startMenu : function() {
-			activateMenu();
+			rootMenu.on();
 		}
 	};
 });
-
-function activateMenu() {
-	rootMenu.on();
-}
-
-function createAndAddMenuTrigger() {
-	var trigger = $("<div id=hbirdMenuTrigger class=" + MENU_TRIGGER + ">").prependTo($("body"));
-	trigger.prepend("<img id=menuTriggerImg src='../../images/metro/appbar.list.png' />");
-}
-
-/**
- * Creates a placeholder div for the menu under the id MENU_PLACEHOLER
- */
-function createAndAddMenuPlaceholder() {
-	var menu = $("<div id=" + MENU_PLACEHOLDER + " style='display: none; position: fixed; top: 64px; left: 4px;' />").prependTo($("body"));
-	menuList = $("<ul>").appendTo(menu);
-}
-
-/**
- * Creates, adds, and switches on the jpanel menu to the MENU_PLACEHOLDER div.
- */
-function attachMenu() {
-	rootMenu = $.jPanelMenu({
-		menu : "#" + MENU_PLACEHOLDER
-	});
-}
-
-function addMenuItem(element) {
-	console.log("Adding menu " + element);
-	menuList.append($("<li>" + element + "</li>"));
-}
 
 
--- src/application/web-client/src/main/webapp/pages/globe/globe.js
+++ src/application/web-client/src/main/webapp/pages/globe/globe.js
@@ -1,138 +1,162 @@
 requirejs.config({
-    "baseUrl" : "../../js"
+    baseUrl : "../../js",
+    paths : {
+    	"Cesium" : "../Cesium/Cesium",
+    	"jquery.pnotify.min" : "pines/jquery.pnotify.min",
+    	"rootMenu" : "hbird/rootMenu"
+    },
+    shim : {
+    	"jquery.pnotify.min" : {
+    		deps : ["jquery"]
+    	},
+    	"jquery.gracefulWebSocket" : {
+    		deps : ["jquery"]
+    	},
+    	"Cesium" : {
+    		// This version of Cesium has no AMD support so we must export the Global.
+    		exports: "Cesium"
+    	}
+    }
 });
 
 require([
-	"jquery",
-	"../Cesium/Cesium",
-	"json2",
-	"jquery.gracefulWebSocket",
-	"pines/jquery.pnotify.min",
-	"hbird/rootMenu"],
-	function($, cesium, json2, graceWebSocket, pnotify, hbirdMenu) {
-		menu = hbirdMenu;
-		setupWebsocket();
-		setupGlobe();
-		addSatelliteSelection();
-		hbirdMenu.startMenu();
-	}
-);
+"jquery",
+"Cesium",
+"json2",
+"jquery.gracefulWebSocket",
+"jquery.pnotify.min",
+"rootMenu"],
+function($, Cesium, json2, graceWebSocket, pnotify, hbirdMenu) {
 
-// The root URL for the RESTful services
-var host = window.location.hostname;
-var url = "/hbird/halcyon/";
-var rootURL = location.protocol + "//" + host + ":" + location.port + url;
+	// The root URL for the RESTful services
+	var host = window.location.hostname;
+	var url = "/hbird/halcyon/";
+	var rootURL = location.protocol + "//" + host + ":" + location.port + url;
 
-var cesiumViewer;
-var liveTmWebsocket;
-var menu;
+	var cesiumViewer = null;
+	var liveTmWebsocket = null;
+	var satInput = null;
 
-/**
- * Add the add satellite input field to the controls section of the globe page. 
- */
-function addSatelliteSelection() {
 
-	var form = $("<form id=satForm />").submit(function() {
-		loadCzml(satSelectfield.val());
-		return false;
-	});
-	
-	$("<input type=search/>").appendTo(form);
-
-	menu.addMenuItem(form);
-}
-
-/**
- * Requests an orbit propagation (as CZML) for the given satellite name via the
- * Halcyon web service and loads the result into the globe.
- * 
- * @param String
- *            satName the name of the satellite to add to the globe.
- */
-function loadCzml(satName) {
-	var url = rootURL + "navigation/tle/propagate/czml/" + satName;
-	$.getJSON(url, null, function(data, textStatus, jqXHR) {
-		var czml = jQuery.parseJSON(jqXHR.responseText);
-
-		// Add dynamic CZML data source.
-		var czmlDataSource = new Cesium.CzmlDataSource();
-		czmlDataSource.load(czml, 'Propagation for ' + satName);
-		cesiumViewer.dataSources.add(czmlDataSource);
-
-	}).fail(function() {
-		console.log("Error loading czml propagation for satellite " + satName);
-	});
-}
-
-/**
- * Creates the Cesium viewer and applies default hbird settings.
- */
-function setupGlobe() {
-	cesiumViewer = new Cesium.Viewer('cesiumContainer');
-
-	cesiumViewer.centralBody.terrainProvider = new Cesium.CesiumTerrainProvider({
-		url : 'http://cesium.agi.com/smallterrain'
-	});
-
-	cesiumViewer.centralBody.enableLighting = true;
-
-	// For dynamic object camera lock on
-	cesiumViewer.extend(Cesium.viewerDynamicObjectMixin);
-}
-
-/**
- * Function called when a live telemetry parameter is received from the
- * websocket.
- * 
- * @param parameter
- *            the new tm paramter
- */
-function parameterReceived(parameter) {
-
-}
-
-/**
- * Sets up the web socket and it's callbacks.
- */
-function setupWebsocket() {
-	var wsProtocol;
-
-	if (location.protocol == "http:") {
-		wsProtocol = "ws:";
-	}
-	else {
-		wsProtocol = "wss:";
-	}
-
-	liveTmWebsocket = $.gracefulWebSocket(wsProtocol + "//" + host + ":" + location.port + url + "websocket");
-
-	liveTmWebsocket.onopen = function() {
-		$.pnotify({
-			title : "System message",
-			text : "Websocket connection established.",
-			type : "info",
-			icon : "'picon picon-network-wireless'"
+	/**
+	 * Add the add satellite input field to the controls section of the globe page. 
+	 */
+	function addSatelliteSelection() {
+		var form = $("<form id=satForm />").submit(function() {
+			loadCzml(satSelectfield.val());
+			console.log("Returning false");
+			return false;
 		});
-	};
+		
+		satInput = $("<input type=search placeholder='search for a satellite' />");
 
-	liveTmWebsocket.onerror = function() {
-		$.pnotify({
-			title : "System message",
-			text : "Websocket connection failed. Cannot receive live telemetry.",
-			type : "error",
-			icon : "'picon picon-network-wireless'"
+		hbirdMenu.addMenuItem(satInput, function() {
+			this.loadCzml(satSelectfield.val());
+			console.log("Returning false");
+			return false;
 		});
-	};
+	}
 
-	// When we receive a message from the websocket first check if there are any
-	// widgets on the dashboard,
-	// if so, parse the message into a parameter object and call our handler.
-	liveTmWebsocket.onmessage = function(event) {
-		var message = $.parseJSON(event.data);
-		if (message.id === "LIVE_TM") {
-			if (Object.keys(hidgetMonitorMap).length > 0) {
-				parameterReceived(message.content);
-			}
+	/**
+	 * Requests an orbit propagation (as CZML) for the given satellite name via the
+	 * Halcyon web service and loads the result into the globe.
+	 * 
+	 * @param String
+	 *            satName the name of the satellite to add to the globe.
+	 */
+	function loadCzml(satName) {
+		var url = rootURL + "navigation/tle/propagate/czml/" + satName;
+		$.getJSON(url, null, function(data, textStatus, jqXHR) {
+			var czml = jQuery.parseJSON(jqXHR.responseText);
+
+			// Add dynamic CZML data source.
+			var czmlDataSource = new Cesium.CzmlDataSource();
+			czmlDataSource.load(czml, 'Propagation for ' + satName);
+			cesiumViewer.dataSources.add(czmlDataSource);
+
+		}).fail(function() {
+			console.log("Error loading czml propagation for satellite " + satName);
+		});
+	}
+
+	/**
+	 * Creates the Cesium viewer and applies default hbird settings.
+	 */
+	function setupGlobe() {
+		cesiumViewer = new Cesium.Viewer('cesiumContainer');
+
+		cesiumViewer.centralBody.terrainProvider = new Cesium.CesiumTerrainProvider({
+			url : 'http://cesium.agi.com/smallterrain'
+		});
+
+		cesiumViewer.centralBody.enableLighting = true;
+
+		// For dynamic object camera lock on
+		cesiumViewer.extend(Cesium.viewerDynamicObjectMixin);
+	}
+
+	/**
+	 * Function called when a live telemetry parameter is received from the
+	 * websocket.
+	 * 
+	 * @param parameter
+	 *            the new tm paramter
+	 */
+	function parameterReceived(parameter) {
+
+	}
+
+	/**
+	 * Sets up the web socket and it's callbacks.
+	 */
+	function setupWebsocket() {
+		var wsProtocol;
+
+		if (location.protocol == "http:") {
+			wsProtocol = "ws:";
 		}
-	};
-}
+		else {
+			wsProtocol = "wss:";
+		}
+
+		liveTmWebsocket = $.gracefulWebSocket(wsProtocol + "//" + host + ":" + location.port + url + "websocket");
+
+		liveTmWebsocket.onopen = function() {
+			$.pnotify({
+				title : "System message",
+				text : "Websocket connection established.",
+				type : "info",
+				icon : "'picon picon-network-wireless'"
+			});
+		};
+
+		liveTmWebsocket.onerror = function() {
+			$.pnotify({
+				title : "System message",
+				text : "Websocket connection failed. Cannot receive live telemetry.",
+				type : "error",
+				icon : "'picon picon-network-wireless'"
+			});
+		};
+
+		// When we receive a message from the websocket first check if there are any
+		// widgets on the dashboard,
+		// if so, parse the message into a parameter object and call our handler.
+		liveTmWebsocket.onmessage = function(event) {
+			var message = $.parseJSON(event.data);
+			if (message.id === "LIVE_TM") {
+				if (Object.keys(hidgetMonitorMap).length > 0) {
+					parameterReceived(message.content);
+				}
+			}
+		};
+	}
+
+	setupWebsocket();
+	setupGlobe();
+	addSatelliteSelection();
+	hbirdMenu.startMenu();	
+		
+});
+
+
